9ba367de86aa1444759a93d9cdf1e6ad0feb1b79
Andre Sato <andre.kubagawa@usp.br>
2020-12-02 18:58:19 -0300

    Add React Router

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -3,12 +3,22 @@ import "../stylesheets/application.css";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Store from "./Store";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const GenericPage = (props) => (
+  <h1 className="text-center p-4 text-lg">{props.title}</h1>
+);
+const Blog = () => <GenericPage title="Blog Page" />;
+const Perguntas = () => <GenericPage title="Perguntas Page" />;
+const Noticias = () => <GenericPage title="Noticias Page" />;
+const Contato = () => <GenericPage title="Contato Page" />;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       windowWidth: 0,
+      tab: 0,
     };
     this.updateWindowWidth = this.updateWindowWidth.bind(this);
   }
@@ -29,13 +39,21 @@ class App extends React.Component {
   render() {
     const { windowWidth } = this.state;
     return (
-      <div className="flex flex-col h-screen justify-between overflow-y-scroll">
-        <NavBar width={windowWidth} />
-        <main className="w-full max-w-screen-xl mx-auto flex-grow">
-          <Store />
-        </main>
-        <Footer />
-      </div>
+      <Router>
+        <div className="flex flex-col h-screen justify-between overflow-y-scroll">
+          <NavBar width={windowWidth} />
+          <main className="w-full max-w-screen-xl mx-auto flex-grow">
+            <Switch>
+              <Route path="/" exact component={Store} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/perguntas" component={Perguntas} />
+              <Route path="/noticias" component={Noticias} />
+              <Route path="/contato" component={Contato} />
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   resources :carts, only: :show
   root to: 'store#index'
   resources :products
+
+  get 'blog', to: 'store#index'
+  get 'perguntas', to: 'store#index'
+  get 'noticias', to: 'store#index'
+  get 'contato', to: 'store#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
